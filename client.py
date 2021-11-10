27a858db8a83c670ed8a569588f4bd5e563afca7
davdleet <davdleet1@gmail.com>
2021-11-10 22:15:13 +0900

    starting gui implementation

--- client.py
+++ client.py
@@ -3,25 +3,25 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import socket
 import sys
+import tkinter
 
-HOST = '221.155.194.202'
+HOST = '221.155.194.15'
 PORT = 50007
 
 def main():
     try:
         for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
             af, socktype, proto, canonname, sa = res
-            print(res)
-            print(sa)
+            # create socket
             try:
                 sock = socket.socket(af, socktype, proto)
             except OSError as msg:
+                print(msg)
                 sock = None
                 continue
+            # connect to server socket
             try:
-                print("connecting..")
                 sock.connect(sa)
-                print("after connect")
             except OSError as msg:
                 print(msg)
                 sock.close()
@@ -29,15 +29,24 @@ def main():
                 continue
             except socket.error as e:
                 print("socket")
+            # break for loop if connection was made for IPv4 or IPv6
             break
+        # If sock is still None after for loop, connection to server failed
         if sock is None:
             print('could not open socket')
             sys.exit(1)
+
+        # after successfully connecting to the server socket
         with sock:
-            print("with sock!")
-            sock.sendall(b"hello world")
-            data = sock.recv(1024)
-            print("received ", repr(data))
+            while True:
+                send_msg = str(input("enter something to send: "))
+                send_msg = send_msg.encode('ascii')
+                sock.send(send_msg)
+                recv_msg = sock.recv(1024)
+                print(recv_msg)
+                if recv_msg == b'':
+                    print("connection with server was lost")
+                    break
 
     except socket.error as e:
         print("outer error")
--- server.py
+++ server.py
@@ -4,10 +4,9 @@ import socket
 import sys
 import urllib.request
 import ssl
+import tkinter
 ssl._create_default_https_context = ssl._create_unverified_context
-
 external_ip = urllib.request.urlopen('https://ident.me/').read().decode('utf8')
-
 print(external_ip)
 
 
@@ -15,57 +14,93 @@ HOST = None
 PORT = 50007
 hostname = socket.gethostname()
 local_ip = socket.gethostbyname(hostname)
-print(local_ip)
+window = tkinter.Tk()
+window.geometry("700x700")
+window.mainloop()
+def setup():
+    sock = None
+    for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
+        af, socktype, proto, cannonname, sa = res
+        try:
+            sock = socket.socket(af, socktype, proto)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        except OSError as msg:
+            sock = None
+            continue
+        # socket is created successfully
+        try:
+            sock.bind(sa)
+            sock.listen(1)
+        except OSError as msg:
+            sock.close()
+            sock = None
+            continue
+        break
+    return sock
+
+def start_socket(sock):
+    conn, addr = sock.accept()
+    with conn:
+        print('connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            parsed = data.decode('ascii')
+            print(parsed)
+            if not data:
+                print("breaking!")
+                break
+            conn.send(b"success")
 
 def main():
-    print("server")
-    try:
-        sock = None
-        for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
-            af, socktype, proto, cannonname, sa = res
-            print(res)
-            try:
-                sock = socket.socket(af, socktype, proto)
-                sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            except OSError as msg:
-                sock = None
-                continue
-            # socket is created successfully
-            try:
-                print("sa is: " + str(sa))
-                sock.bind(sa)
-                sock.listen(1)
-            except OSError as msg:
-                sock.close()
-                sock = None
-                continue
-            break
-        if sock is None:
-            print("could not open socket")
-            sys.exit(1)
-        print("HOST,PORT: " + str(HOST), str(PORT))
-        print("waiting to accept..")
-        conn, addr = sock.accept()
-        with conn:
-            print('connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    print("breaking!")
-                    break
-                conn.send(data)
-                parsed = data.decode('ascii')
-                print(parsed)
-        #sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0, fileno=None)
-        #print(HOST, PORT)
-        # print(HOST)
-        #sock.bind((HOST, PORT))
-    except KeyboardInterrupt as e:
-        print("server ended by user")
-    except socket.error as e:
-        print("some error occurred")
+    window.mainloop()
+    sock = setup()
+    if sock is None:
+        print("could not open socket")
+        sys.exit(1)
+    start_socket(sock)
+    # window.title("Chatting Program")
+    # window.mainloop()
+    # try:
+    #     sock = None
+    #     for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
+    #         af, socktype, proto, cannonname, sa = res
+    #         try:
+    #             sock = socket.socket(af, socktype, proto)
+    #             sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    #         except OSError as msg:
+    #             sock = None
+    #             continue
+    #         # socket is created successfully
+    #         try:
+    #             sock.bind(sa)
+    #             sock.listen(1)
+    #         except OSError as msg:
+    #             sock.close()
+    #             sock = None
+    #             continue
+    #         break
+    #     if sock is None:
+    #         print("could not open socket")
+    #         sys.exit(1)
+    #     print("HOST,PORT: " + str(HOST), str(PORT))
+    #     print("waiting to accept..")
+    #     conn, addr = sock.accept()
+    #     with conn:
+    #         print('connected by', addr)
+    #         while True:
+    #             data = conn.recv(1024)
+    #             parsed = data.decode('ascii')
+    #             print(parsed)
+    #             if not data:
+    #                 print("breaking!")
+    #                 break
+    #             conn.send(b"success")
+    # except KeyboardInterrupt as e:
+    #     print("server ended by user")
+    # except socket.error as e:
+    #     print("some error occurred")
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+#    main()
