2447cf84b9b7fd0834646ba2d0feeb16f88a11cc
davdleet <davdleet1@gmail.com>
2021-11-11 20:28:49 +0900

    using thread to run gui and server in parallel

--- server.py
+++ server.py
@@ -2,15 +2,16 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import socket
 import sys
+import threading
 import urllib.request
 import ssl
 import tkinter
 from tkinter import *
+from threading import Thread
 
 ssl._create_default_https_context = ssl._create_unverified_context
 external_ip = urllib.request.urlopen('https://ident.me/').read().decode('utf8')
-print(external_ip)
-
+#print(external_ip)
 
 HOST = None
 PORT = 50007
@@ -18,54 +19,86 @@ hostname = socket.gethostname()
 local_ip = socket.gethostbyname(hostname)
 window = tkinter.Tk()
 
-def gui():
+sock = None
+
+
+def gui_setup():
     window.geometry("700x700")
-    window['background']='#252229'
-    window.resizable(False, False)
+    window['background'] = '#252229'
+    window.resizable(True, True)
     window.title("Socket Chatting")
 
-    title = tkinter.Label(window, bg='#252229',fg='white', font=("Lucida Grande", 25), text="Start a chatting Server", relief="solid")
+    title = tkinter.Label(window, bg='#252229', fg='white', font=("Lucida Grande", 25), text="Start a chatting Server",
+                          relief="solid")
     title.pack(pady=20)
 
-    label1 = Label(window, bg='#252229',fg='white', relief="solid")
+    label1 = Label(window, bg='#252229', fg='white', relief="solid")
     label1.pack(fill="both", padx=20)
 
-    server_ip = Label(label1, bg='#252229',fg='white', font=("Lucida Grande", 18), text="Server IP: ", relief="solid")
+    server_ip = Label(label1, bg='#252229', fg='white', font=("Lucida Grande", 18), text="Server IP: ", relief="solid")
     server_ip.pack(side="left")
 
-    server_ip_value = Label(label1,bg='#252229',fg='white', font=("Lucida Grande", 18), text=external_ip, relief="solid")
+    server_ip_value = Label(label1, bg='#252229', fg='white', font=("Lucida Grande", 18), text=external_ip,
+                            relief="solid")
     server_ip_value.pack(side="left")
 
-    label2 = Label(window, bg='#252229',fg='white', relief="solid")
+    label2 = Label(window, bg='#252229', fg='white', relief="solid")
     label2.pack(fill="both", padx=20)
 
-    port = Label(label2, bg='#252229',fg='white', font=("Lucida Grande", 18), text="Port: ", relief="solid")
+    port = Label(label2, bg='#252229', fg='white', font=("Lucida Grande", 18), text="Port: ", relief="solid")
     port.pack(side="left")
 
-    port_value = Entry(label2, bg='#252229',fg='white', bd=0, font=("Lucida Grande", 18), text="Port: ")
+    port_value = Entry(label2, bg='#252229', fg='white', bd=0, font=("Lucida Grande", 18), text="Port: ")
     port_value.pack(side="left")
 
-    label3 = Label(window, bg='#252229',fg='white', relief="solid")
-    label3.pack(fill="both", padx=20)
+    label3 = Label(window, bg='#252229', fg='white', relief="solid")
+    label3.pack(fill="x", padx=20)
 
-    password = Label(label3, bg='#252229',fg='white', font=("Lucida Grande", 18), text="Password: ", relief="solid")
+    password = Label(label3, bg='#252229', fg='white', font=("Lucida Grande", 18), text="Password: ", relief="solid")
     password.pack(side="left")
 
-    password_value = Entry(label3, bg='#252229',fg='white', bd=0, font=("Lucida Grande", 18), text="Password: ")
+    password_value = Entry(label3, bg='#252229', fg='white', bd=0, font=("Lucida Grande", 18), text="Password: ")
     password_value.pack(side="left")
 
-    label4 = Label(window, height= 27, width=100, bg='#252229',fg='white', relief="solid", text="hello world\nhello")
-    label4.pack(fill="x",side="left",padx=20, pady= 20, anchor="n")
+    label4 = Label(window, bg='#252229', fg='white', relief="solid", anchor="w", font=("Lucida Grande", 18),
+                   text="Connected Clients")
+    label4.pack(fill="x", padx=20, pady=10)
 
-    button1 = tkinter.Button(window, command=None, width=50, height="50", text="press")
-    button1.pack()
+    label5 = Label(window, bg='#252229', fg='white', relief="solid")
+    label5.pack(fill="x", padx=20)
 
+    list = Text(label5, bg='#252229', fg='white', width=100, height=24, wrap=None, relief="solid")
+    list.pack(padx=20)
+
+    for i in range(200):
+        list.insert(END, "this is some text" + str(i) + "\n")
+
+    #scroll = Scrollbar(label5, orient="vertical")
+    #scroll.pack(side="right")
+
+    #label5.config(xscrollcommand = scroll.set)
+
+    label6 = tkinter.Label(window, height=100)
+    label6.pack(side="right", padx=20, pady=20, fill="x")
+
+    start_button = tkinter.Button(label6, font=("Lucida Grande", 18), command=Thread(target=start_server).start, width=3, height=1, text="start")
+    start_button.pack(side="right", ipadx=3, ipady=3)
+
+    stop_button = tkinter.Button(label6, font=("Lucida Grande", 18), command=None, width=3, height=1, text="stop")
+    stop_button.pack(side="right", padx=20, ipadx=3, ipady=3)
 
     window.mainloop()
 
+def start_server():
+    setup()
+    if sock is None:
+        print("could not open socket")
+        sys.exit(1)
+    start_socket()
+    return
 
 def setup():
-    sock = None
+    global sock
     for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
         af, socktype, proto, cannonname, sa = res
         try:
@@ -85,7 +118,9 @@ def setup():
         break
     return sock
 
-def start_socket(sock):
+
+def start_socket():
+    print("server open!")
     conn, addr = sock.accept()
     with conn:
         print('connected by', addr)
@@ -98,13 +133,10 @@ def start_socket(sock):
                 break
             conn.send(b"success")
 
+
 def main():
+    print("setup over")
     window.mainloop()
-    sock = setup()
-    if sock is None:
-        print("could not open socket")
-        sys.exit(1)
-    start_socket(sock)
     # window.title("Chatting Program")
     # window.mainloop()
     # try:
@@ -150,5 +182,5 @@ def main():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-#    main()
-    gui()
+    #    main()
+    gui_setup()
