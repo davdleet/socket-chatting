0c664e061d1ffff558aa959a30d3d76e656632d5
davdleet <davdleet1@gmail.com>
2021-11-11 16:32:08 +0900

    set color

--- server.py
+++ server.py
@@ -24,37 +24,37 @@ def gui():
     window.resizable(False, False)
     window.title("Socket Chatting")
 
-    title = tkinter.Label(window, bg='#252229', font=("Lucida Grande", 25), text="Start a chatting Server", relief="solid")
+    title = tkinter.Label(window, bg='#252229',fg='white', font=("Lucida Grande", 25), text="Start a chatting Server", relief="solid")
     title.pack(pady=20)
 
-    label1 = Label(window, bg='#252229', relief="solid")
+    label1 = Label(window, bg='#252229',fg='white', relief="solid")
     label1.pack(fill="both", padx=20)
 
-    server_ip = Label(label1, bg='#252229', font=("Lucida Grande", 18), text="Server IP: ", relief="solid")
+    server_ip = Label(label1, bg='#252229',fg='white', font=("Lucida Grande", 18), text="Server IP: ", relief="solid")
     server_ip.pack(side="left")
 
-    server_ip_value = Label(label1,bg='#252229', font=("Lucida Grande", 18), text=external_ip, relief="solid")
+    server_ip_value = Label(label1,bg='#252229',fg='white', font=("Lucida Grande", 18), text=external_ip, relief="solid")
     server_ip_value.pack(side="left")
 
-    label2 = Label(window, bg='#252229', relief="solid")
+    label2 = Label(window, bg='#252229',fg='white', relief="solid")
     label2.pack(fill="both", padx=20)
 
-    port = Label(label2, bg='#252229', font=("Lucida Grande", 18), text="Port: ", relief="solid")
+    port = Label(label2, bg='#252229',fg='white', font=("Lucida Grande", 18), text="Port: ", relief="solid")
     port.pack(side="left")
 
-    port_value = Entry(label2, bg='#252229', bd=0, font=("Lucida Grande", 18), text="Password: ")
+    port_value = Entry(label2, bg='#252229',fg='white', bd=0, font=("Lucida Grande", 18), text="Port: ")
     port_value.pack(side="left")
 
-    label3 = Label(window, bg='#252229', relief="solid")
+    label3 = Label(window, bg='#252229',fg='white', relief="solid")
     label3.pack(fill="both", padx=20)
 
-    password = Label(label3, bg='#252229', font=("Lucida Grande", 18), text="Password: ", relief="solid")
+    password = Label(label3, bg='#252229',fg='white', font=("Lucida Grande", 18), text="Password: ", relief="solid")
     password.pack(side="left")
 
-    password_value = Entry(label3, bg='#252229', bd=0, font=("Lucida Grande", 18), text="Password: ")
+    password_value = Entry(label3, bg='#252229',fg='white', bd=0, font=("Lucida Grande", 18), text="Password: ")
     password_value.pack(side="left")
 
-    label4 = Label(window, height= 27, width=100, bg='#252229', relief="solid", text="hello world\nhello")
+    label4 = Label(window, height= 27, width=100, bg='#252229',fg='white', relief="solid", text="hello world\nhello")
     label4.pack(fill="x",side="left",padx=20, pady= 20, anchor="n")
 
     button1 = tkinter.Button(window, command=None, width=50, height="50", text="press")
