1ec29d9af97b64d04818f5642f91e1f1f284f3db
davdleet <davdleet1@gmail.com>
2021-11-12 16:58:53 +0900

    using thread to run gui and server in parallel

--- server.py
+++ server.py
@@ -7,22 +7,91 @@ import urllib.request
 import ssl
 import tkinter
 from tkinter import *
+from tkinter import messagebox
 from threading import Thread
 
 ssl._create_default_https_context = ssl._create_unverified_context
 external_ip = urllib.request.urlopen('https://ident.me/').read().decode('utf8')
-#print(external_ip)
+# print(external_ip)
 
 HOST = None
 PORT = 50007
 hostname = socket.gethostname()
 local_ip = socket.gethostbyname(hostname)
 window = tkinter.Tk()
-
 sock = None
+myGui = None
+threads = []
+
+def join_threads():
+    for thread in threads:
+        print("joining..")
+        thread.join()
+
+def start_server():
+    setup()
+    if sock is None:
+        print("could not open socket")
+        sys.exit(1)
+    start_socket()
+    return
 
 
-def gui_setup():
+def setup():
+    global sock
+    global PORT
+    message = "nothing"
+    try:
+        PORT = myGui.port_value.get()
+        if PORT == "":
+            raise Exception("Enter a valid port number")
+        #if PORT is None:
+            # raise Exception("Enter a valid port number")
+        for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
+            af, socktype, proto, cannonname, sa = res
+            try:
+                sock = socket.socket(af, socktype, proto)
+                sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            except OSError as msg:
+                sock = None
+                continue
+            # socket is created successfully
+            try:
+                sock.bind(sa)
+                sock.listen(1)
+            except OSError as msg:
+                sock.close()
+                sock = None
+                continue
+            break
+        return sock
+    except Exception as e:
+        messagebox.showerror("Error!", e)
+        join_threads()
+        sys.exit(1)
+
+
+def add_to_list(client_info):
+    None
+
+
+def start_socket():
+    print("server open!")
+
+    conn, addr = sock.accept()
+    with conn:
+        add_to_list(addr)
+        print('connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            parsed = data.decode('ascii')
+            print(parsed)
+            if not data:
+                print("breaking!")
+                break
+            conn.send(b"success")
+
+class Gui:
     window.geometry("700x700")
     window['background'] = '#252229'
     window.resizable(True, True)
@@ -64,123 +133,52 @@ def gui_setup():
                    text="Connected Clients")
     label4.pack(fill="x", padx=20, pady=10)
 
-    label5 = Label(window, bg='#252229', fg='white', relief="solid")
+    label5 = Label(window, height=100, bg='#252229', fg='white', relief="solid")
     label5.pack(fill="x", padx=20)
 
-    list = Text(label5, bg='#252229', fg='white', width=100, height=24, wrap=None, relief="solid")
-    list.pack(padx=20)
+    list = Text(label5, bg='#252229', fg='white', width=89, height=25, wrap=None, relief="solid")
+    list.pack(side="left")
 
-    for i in range(200):
-        list.insert(END, "this is some text" + str(i) + "\n")
+    for line in range(100):
+        list.insert(END, "This is line number " + str(line) + "\n")
 
-    #scroll = Scrollbar(label5, orient="vertical")
-    #scroll.pack(side="right")
+    scroll = Scrollbar(label5, orient="vertical", command=list.yview)
+    scroll.pack(side='right', fill=Y)
 
-    #label5.config(xscrollcommand = scroll.set)
+    list.config(yscrollcommand=scroll.set)
 
-    label6 = tkinter.Label(window, height=100)
+    list.insert(tkinter.END, "hello")
+
+    # scroll = Scrollbar(label5, orient="vertical")
+    # scroll.pack(side="right")
+
+    # label5.config(xscrollcommand = scroll.set)
+
+    label6 = tkinter.Label(window, height=80)
     label6.pack(side="right", padx=20, pady=20, fill="x")
 
-    start_button = tkinter.Button(label6, font=("Lucida Grande", 18), command=Thread(target=start_server).start, width=3, height=1, text="start")
+    t = Thread(target=start_server)
+    threads.append(t)
+    start_button = tkinter.Button(label6, font=("Lucida Grande", 18), command=t.start,
+                                  width=3, height=1, text="start")
     start_button.pack(side="right", ipadx=3, ipady=3)
 
     stop_button = tkinter.Button(label6, font=("Lucida Grande", 18), command=None, width=3, height=1, text="stop")
     stop_button.pack(side="right", padx=20, ipadx=3, ipady=3)
 
-    window.mainloop()
+    def start(self):
+        window.mainloop()
 
-def start_server():
-    setup()
-    if sock is None:
-        print("could not open socket")
-        sys.exit(1)
-    start_socket()
-    return
-
-def setup():
-    global sock
-    for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
-        af, socktype, proto, cannonname, sa = res
-        try:
-            sock = socket.socket(af, socktype, proto)
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        except OSError as msg:
-            sock = None
-            continue
-        # socket is created successfully
-        try:
-            sock.bind(sa)
-            sock.listen(1)
-        except OSError as msg:
-            sock.close()
-            sock = None
-            continue
-        break
-    return sock
+myGui = Gui()
 
 
-def start_socket():
-    print("server open!")
-    conn, addr = sock.accept()
-    with conn:
-        print('connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            parsed = data.decode('ascii')
-            print(parsed)
-            if not data:
-                print("breaking!")
-                break
-            conn.send(b"success")
-
+def add_to_list(client_info):
+    myGui.list.insert(tkinter.END, str(client_info))
 
 def main():
-    print("setup over")
-    window.mainloop()
-    # window.title("Chatting Program")
-    # window.mainloop()
-    # try:
-    #     sock = None
-    #     for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
-    #         af, socktype, proto, cannonname, sa = res
-    #         try:
-    #             sock = socket.socket(af, socktype, proto)
-    #             sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    #         except OSError as msg:
-    #             sock = None
-    #             continue
-    #         # socket is created successfully
-    #         try:
-    #             sock.bind(sa)
-    #             sock.listen(1)
-    #         except OSError as msg:
-    #             sock.close()
-    #             sock = None
-    #             continue
-    #         break
-    #     if sock is None:
-    #         print("could not open socket")
-    #         sys.exit(1)
-    #     print("HOST,PORT: " + str(HOST), str(PORT))
-    #     print("waiting to accept..")
-    #     conn, addr = sock.accept()
-    #     with conn:
-    #         print('connected by', addr)
-    #         while True:
-    #             data = conn.recv(1024)
-    #             parsed = data.decode('ascii')
-    #             print(parsed)
-    #             if not data:
-    #                 print("breaking!")
-    #                 break
-    #             conn.send(b"success")
-    # except KeyboardInterrupt as e:
-    #     print("server ended by user")
-    # except socket.error as e:
-    #     print("some error occurred")
+    Gui.start(myGui)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #    main()
-    gui_setup()
+    main()
