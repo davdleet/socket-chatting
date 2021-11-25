9a09f2ee896be77bedad7afb5c9412a89f9f6492
davdleet <davdleet1@gmail.com>
2021-11-25 22:41:48 +0900

    changed gui to class instances

--- client.py
+++ client.py
@@ -6,16 +6,16 @@ import tkinter
 from tkinter import *
 import threading
 from threading import Thread
-window = tkinter.Tk()
 
-gui=None
-
+gui = None
+sock=None
 
 def connect_to_server():
     HOST = gui.server_ip_value.get()
     PORT = gui.port_value.get()
     password = gui.password_value.get()
     username = gui.username_value.get()
+    global sock
     try:
         for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
             af, socktype, proto, canonname, sa = res
@@ -41,7 +41,7 @@ def connect_to_server():
         # If sock is still None after for loop, connection to server failed
         if sock is None:
             print('could not open socket')
-            return
+            return 2
 
         # after successfully connecting to the server socket
         with sock:
@@ -56,21 +56,15 @@ def connect_to_server():
                 pw_verified = True
             if not pw_verified:
                 print(decoded_pw_check_msg)
-                return
+                return 1
 
             # send username to server
             encoded_username = username.encode('ascii')
             sock.send(encoded_username)
-
-            while True:
-                send_msg = str(input("enter something to send: "))
-                send_msg = send_msg.encode('ascii')
-                sock.send(send_msg)
-                recv_msg = sock.recv(1024)
-                print(recv_msg)
-                if recv_msg == b'Server Closed':
-                    print("connection with server was lost")
-                    break
+            t = Thread(target=chat)
+            t.start()
+            return 0
+            # chat(sock)
 
     except socket.error as e:
         print("outer error")
@@ -78,90 +72,125 @@ def connect_to_server():
     except KeyboardInterrupt as e:
         print("client closed by user")
 
+
+def chat():
+    global sock
+    while True:
+        send_msg = str(input("enter something to send: "))
+        send_msg = send_msg.encode('ascii')
+        sock.send(send_msg)
+        recv_msg = sock.recv(1024)
+        print(recv_msg)
+        if recv_msg == b'Server Closed':
+            print("connection with server was lost")
+            break
+
+
 class JoinGui:
-    server_ip_value = None
-    port_value = None
-    password_value = None
-    username_value = None
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.window.geometry("450x300")
+        self.window['background'] = '#252229'
+        self.window.resizable(True, True)
+        self.window.title("Socket Chatting Client")
+        self.title = tkinter.Label(self.window, bg='#252229', fg='white', font=("Lucida Grande", 25),
+                                   text="Join a chatting server!",
+                                   relief="solid")
+        self.title.pack(pady=20)
 
+        self.label1 = Label(self.window, bg='#252229', fg='white', relief="solid")
+        self.label1.pack(fill="both", padx=20)
 
-    window.geometry("450x300")
-    window['background'] = '#252229'
-    window.resizable(True, True)
-    window.title("Socket Chatting Client")
-    title = tkinter.Label(window, bg='#252229', fg='white', font=("Lucida Grande", 25),
-                          text="Join a chatting server!",
+        self.server_ip = Label(self.label1, bg='#252229', fg='white', font=("Lucida Grande", 18), text="Server IP: ",
+                               relief="solid")
+        self.server_ip.pack(side="left")
+
+        self.server_ip_value = Entry(self.label1, bg='#252229', fg='white', bd=0, font=("Lucida Grande", 18))
+        self.server_ip_value.pack(side="right")
+
+        self.label2 = Label(self.window, bg='#252229', fg='white', relief="solid")
+        self.label2.pack(fill="both", padx=20)
+
+        self.port = Label(self.label2, bg='#252229', fg='white', font=("Lucida Grande", 18), text="Port: ",
                           relief="solid")
-    title.pack(pady=20)
+        self.port.pack(side="left")
 
-    label1 = Label(window, bg='#252229', fg='white', relief="solid")
-    label1.pack(fill="both", padx=20)
+        self.port_value = Entry(self.label2, bg='#252229', fg='white', bd=0, font=("Lucida Grande", 18))
+        self.port_value.pack(side="right")
 
-    server_ip = Label(label1, bg='#252229', fg='white', font=("Lucida Grande", 18), text="Server IP: ",
-                      relief="solid")
-    server_ip.pack(side="left")
+        self.label3 = Label(self.window, bg='#252229', fg='white', relief="solid")
+        self.label3.pack(fill="x", padx=20)
 
-    server_ip_value = Entry(label1, bg='#252229', fg='white', bd=0, font=("Lucida Grande", 18))
-    server_ip_value.pack(side="right")
+        self.password = Label(self.label3, bg='#252229', fg='white', font=("Lucida Grande", 18), text="Password: ",
+                              relief="solid")
+        self.password.pack(side="left")
 
-    label2 = Label(window, bg='#252229', fg='white', relief="solid")
-    label2.pack(fill="both", padx=20)
+        self.password_value = Entry(self.label3, bg='#252229', fg='white', bd=0, font=("Lucida Grande", 18))
+        self.password_value.pack(side="right")
 
-    port = Label(label2, bg='#252229', fg='white', font=("Lucida Grande", 18), text="Port: ", relief="solid")
-    port.pack(side="left")
+        self.label4 = Label(self.window, bg='#252229', fg='white', relief="solid")
+        self.label4.pack(fill="x", padx=20)
 
-    port_value = Entry(label2, bg='#252229', fg='white', bd=0, font=("Lucida Grande", 18))
-    port_value.pack(side="right")
+        self.username = Label(self.label4, bg='#252229', fg='white', font=("Lucida Grande", 18), text="Username: ",
+                              relief="solid")
+        self.username.pack(side="left")
 
-    label3 = Label(window, bg='#252229', fg='white', relief="solid")
-    label3.pack(fill="x", padx=20)
+        self.username_value = Entry(self.label4, bg='#252229', fg='white', bd=0, font=("Lucida Grande", 18))
+        self.username_value.pack(side="right")
 
-    password = Label(label3, bg='#252229', fg='white', font=("Lucida Grande", 18), text="Password: ",
-                     relief="solid")
-    password.pack(side="left")
+        self.label5 = tkinter.Label(self.window, height=80)
+        self.label5.pack(side="right", padx=20, pady=20, fill="x")
 
-    password_value = Entry(label3, bg='#252229', fg='white', bd=0, font=("Lucida Grande", 18))
-    password_value.pack(side="right")
+        self.start_button = tkinter.Button(self.label5, font=("Lucida Grande", 18), command=self.press_start_button,
+                                           width=3, height=1,
+                                           text="Join")
+        self.start_button.pack(side="right", ipadx=3, ipady=3)
 
-    label4 = Label(window, bg='#252229', fg='white', relief="solid")
-    label4.pack(fill="x", padx=20)
+        self.reset_button = tkinter.Button(self.label5, font=("Lucida Grande", 18), command=self.reset_input, width=3,
+                                           height=1,
+                                           text="Reset")
+        self.reset_button.pack(side="right", padx=20, ipadx=3, ipady=3)
 
-    username = Label(label4, bg='#252229', fg='white', font=("Lucida Grande", 18), text="Username: ",
-                     relief="solid")
-    username.pack(side="left")
+    def press_start_button(self):
+        result = connect_to_server()
+        if result == 1:
+            print("Your password was wrong")
+        elif result > 0:
+            print("There was some error in connecting")
+        elif result == 0:
+            print("connection was successful")
+            self.window.destroy()
+            open_chat_gui()
 
-    username_value = Entry(label4, bg='#252229', fg='white', bd=0, font=("Lucida Grande", 18))
-    username_value.pack(side="right")
-
-    label5 = tkinter.Label(window, height=80)
-    label5.pack(side="right", padx=20, pady=20, fill="x")
-
-    start_button = tkinter.Button(label5, font=("Lucida Grande", 18), command=connect_to_server, width=3, height=1,
-                                  text="Join")
-    start_button.pack(side="right", ipadx=3, ipady=3)
-
-    stop_button = tkinter.Button(label5, font=("Lucida Grande", 18), command=None, width=3, height=1,
-                                 text="Reset")
-    stop_button.pack(side="right", padx=20, ipadx=3, ipady=3)
+    def reset_input(self):
+        self.server_ip_value.delete(0, END)
+        self.port_value.delete(0, END)
+        self.password_value.delete(0, END)
+        self.username_value.delete(0, END)
 
     def start(self):
-        window.mainloop()
+        self.window.mainloop()
+
+
+def open_chat_gui():
+    chatgui = ChatGui()
+    chatgui.start()
 
 
 class ChatGui:
-    def setup(self):
-        window.geometry("700x700")
-        window['background'] = '#252229'
-        window.resizable(True, True)
-        window.title("Socket Chatting Client")
-        title = tkinter.Label(window, bg='#252229', fg='white', font=("Lucida Grande", 25),
-                              text="Join a chatting server!",
-                              relief="solid")
-        title.pack(pady=20)
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.window.geometry("700x700")
+        self.window['background'] = '#252229'
+        self.window.resizable(True, True)
+        self.window.title("Socket Chatting Client")
+        self.title = tkinter.Label(self.window, bg='#252229', fg='white', font=("Lucida Grande", 25),
+                                   text="Join a chatting server!",
+                                   relief="solid")
+        self.title.pack(pady=20)
 
     def start(self):
-        window.mainloop()
-
+        self.window.mainloop()
 
 
 def main():
--- server.py
+++ server.py
@@ -18,7 +18,7 @@ HOST = None
 PORT = None
 hostname = socket.gethostname()
 local_ip = socket.gethostbyname(hostname)
-window = tkinter.Tk()
+# window = tkinter.Tk()
 sock = None
 serverGui = None
 threads = []
@@ -34,16 +34,6 @@ def join_threads():
         print("joining..")
         thread.join()
 
-
-def start_server():
-    setup()
-    if sock is None:
-        print("could not open socket")
-        return
-        print("test")
-    print("starting socket...")
-    start_socket()
-    return
 
 
 def setup():
@@ -81,6 +71,16 @@ def setup():
         messagebox.showerror("Error!", e)
         sys.exit(1)
 
+
+def start_server():
+    setup()
+    if sock is None:
+        print("could not open socket")
+        return
+        print("test")
+    print("starting socket...")
+    start_socket()
+    return
 
 def add_to_list(client_info):
     None
@@ -184,75 +184,77 @@ def stop_server_button():
 
 
 class Gui:
-    window.geometry("700x700")
-    window['background'] = '#252229'
-    window.resizable(True, True)
-    window.title("Socket Chatting Server")
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.window.geometry("700x700")
+        self.window['background'] = '#252229'
+        self.window.resizable(True, True)
+        self.window.title("Socket Chatting Server")
 
-    title = tkinter.Label(window, bg='#252229', fg='white', font=("Lucida Grande", 25), text="Start a chatting Server",
-                          relief="solid")
-    title.pack(pady=20)
+        self.title = tkinter.Label(self.window, bg='#252229', fg='white', font=("Lucida Grande", 25), text="Start a chatting Server",
+                              relief="solid")
+        self.title.pack(pady=20)
 
-    label1 = Label(window, bg='#252229', fg='white', relief="solid")
-    label1.pack(fill="both", padx=20)
+        self.label1 = Label(self.window, bg='#252229', fg='white', relief="solid")
+        self.label1.pack(fill="both", padx=20)
 
-    server_ip = Label(label1, bg='#252229', fg='white', font=("Lucida Grande", 18), text="Server IP: ", relief="solid")
-    server_ip.pack(side="left")
+        self.server_ip = Label(self.label1, bg='#252229', fg='white', font=("Lucida Grande", 18), text="Server IP: ", relief="solid")
+        self.server_ip.pack(side="left")
 
-    server_ip_value = Label(label1, bg='#252229', fg='white', font=("Lucida Grande", 18), text=external_ip,
-                            relief="solid")
-    server_ip_value.pack(side="left")
+        self.server_ip_value = Label(self.label1, bg='#252229', fg='white', font=("Lucida Grande", 18), text=external_ip,
+                                relief="solid")
+        self.server_ip_value.pack(side="left")
 
-    label2 = Label(window, bg='#252229', fg='white', relief="solid")
-    label2.pack(fill="both", padx=20)
+        self.label2 = Label(self.window, bg='#252229', fg='white', relief="solid")
+        self.label2.pack(fill="both", padx=20)
 
-    port = Label(label2, bg='#252229', fg='white', font=("Lucida Grande", 18), text="Port: ", relief="solid")
-    port.pack(side="left")
+        self.port = Label(self.label2, bg='#252229', fg='white', font=("Lucida Grande", 18), text="Port: ", relief="solid")
+        self.port.pack(side="left")
 
-    port_value = Entry(label2, bg='#252229', fg='white', bd=0, font=("Lucida Grande", 18), text="Port: ")
-    port_value.pack(side="left")
+        self.port_value = Entry(self.label2, bg='#252229', fg='white', bd=0, font=("Lucida Grande", 18), text="Port: ")
+        self.port_value.pack(side="left")
 
-    label3 = Label(window, bg='#252229', fg='white', relief="solid")
-    label3.pack(fill="x", padx=20)
+        self.label3 = Label(self.window, bg='#252229', fg='white', relief="solid")
+        self.label3.pack(fill="x", padx=20)
 
-    password = Label(label3, bg='#252229', fg='white', font=("Lucida Grande", 18), text="Password: ", relief="solid")
-    password.pack(side="left")
+        self.password = Label(self.label3, bg='#252229', fg='white', font=("Lucida Grande", 18), text="Password: ", relief="solid")
+        self.password.pack(side="left")
 
-    password_value = Entry(label3, bg='#252229', fg='white', bd=0, font=("Lucida Grande", 18), text="Password: ")
-    password_value.pack(side="left")
+        self.password_value = Entry(self.label3, bg='#252229', fg='white', bd=0, font=("Lucida Grande", 18), text="Password: ")
+        self.password_value.pack(side="left")
 
-    label4 = Label(window, bg='#252229', fg='white', relief="solid", anchor="w", font=("Lucida Grande", 18),
-                   text="Connected Clients")
-    label4.pack(fill="x", padx=20, pady=10)
+        self.label4 = Label(self.window, bg='#252229', fg='white', relief="solid", anchor="w", font=("Lucida Grande", 18),
+                    text="Connected Clients")
+        self.label4.pack(fill="x", padx=20, pady=10)
 
-    label5 = Label(window, height=100, bg='#252229', fg='white', relief="solid")
-    label5.pack(padx=20)
+        self.label5 = Label(self.window, height=100, bg='#252229', fg='white', relief="solid")
+        self.label5.pack(padx=20)
 
-    list = Text(label5, bg='#252229', fg='white', width=69, height=22, wrap=None, font=("TkFixedFont", 14),
-                relief="solid")
-    list.pack(side="left")
+        self.list = Text(self.label5, bg='#252229', fg='white', width=69, height=22, wrap=None, font=("TkFixedFont", 14),
+                    relief="solid")
+        self.list.pack(side="left")
 
-    top_row = "{:<43}".format("Address") + "{:<20}".format("Port") + "{:<30}".format("Username") + "\n\n"
-    list.insert(tkinter.END, top_row)
+        self.top_row = "{:<43}".format("Address") + "{:<20}".format("Port") + "{:<30}".format("Username") + "\n\n"
+        self.list.insert(tkinter.END, self.top_row)
 
-    scroll = Scrollbar(label5, orient="vertical", command=list.yview)
-    scroll.pack(side='right', fill=Y)
+        self.scroll = Scrollbar(self.label5, orient="vertical", command=self.list.yview)
+        self.scroll.pack(side='right', fill=Y)
 
-    list.config(yscrollcommand=scroll.set)
+        self.list.config(yscrollcommand=self.scroll.set)
 
-    label6 = tkinter.Label(window, height=80)
-    label6.pack(side="right", padx=20, pady=20, fill="x")
+        self.label6 = tkinter.Label(self.window, height=80)
+        self.label6.pack(side="right", padx=20, pady=20, fill="x")
 
-    start_button = tkinter.Button(label6, font=("Lucida Grande", 18), command=start_threads, width=3, height=1,
-                                  text="start")
-    start_button.pack(side="right", ipadx=3, ipady=3)
+        self.start_button = tkinter.Button(self.label6, font=("Lucida Grande", 18), command=start_threads, width=3, height=1,
+                                      text="start")
+        self.start_button.pack(side="right", ipadx=3, ipady=3)
 
-    stop_button = tkinter.Button(label6, font=("Lucida Grande", 18), command=stop_server_button, width=3, height=1,
-                                 text="stop")
-    stop_button.pack(side="right", padx=20, ipadx=3, ipady=3)
+        self.stop_button = tkinter.Button(self.label6, font=("Lucida Grande", 18), command=stop_server_button, width=3, height=1,
+                                     text="stop")
+        self.stop_button.pack(side="right", padx=20, ipadx=3, ipady=3)
 
     def start(self):
-        window.mainloop()
+        self.window.mainloop()
 
 
 serverGui = Gui()
