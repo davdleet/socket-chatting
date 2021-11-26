40b8c5b49b69c0c293e715476cb9abd9613cfe7f
davdleet <davdleet1@gmail.com>
2021-11-26 22:53:14 +0900

    remove progress bar

--- server.py
+++ server.py
@@ -188,7 +188,6 @@ def recv_file(conn, received):
     # convert to integer
     filesize = int(filesize)
 
-    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
     with open(filename, "wb") as f:
         while True:
             # read 1024 bytes from the socket (receive)
@@ -200,7 +199,6 @@ def recv_file(conn, received):
             # write to the file the bytes we just received
             f.write(bytes_read)
             # update the progress bar
-            progress.update(len(bytes_read))
 
 def get_new_thread():
     t = Thread(target=start_server)
