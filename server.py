d05869be5b7a7b43faee94947c29fba0448b7ef5
davdleet <davdleet1@gmail.com>
2021-11-27 01:47:22 +0900

    nvm

--- server.py
+++ server.py
@@ -203,17 +203,15 @@ def send_file():
     None
 
 def recv_file(conn, received):
-    copy_sock = copy.deepcopy(conn)
     filename, filesize = received.split(SEPARATOR)
     filename = os.path.basename(filename)
     filesize = int(filesize)
     recv_times = int(filesize / BUFFER_SIZE + 1)
     with open(filename, "wb") as f:
         for i in range(0, recv_times):
-            bytes_read = copy_sock.recv(BUFFER_SIZE)
+            bytes_read = conn.recv(BUFFER_SIZE)
             f.write(bytes_read)
         f.close()
-    copy_sock.close()
 
 def get_new_thread():
     t = Thread(target=start_server)
