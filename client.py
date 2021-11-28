a9b4aaa479c6b2612df2f4e77b477f72663ada0e
davdleet <davdleet1@gmail.com>
2021-11-28 16:37:07 +0900

    client side receive file

--- client.py
+++ client.py
@@ -7,7 +7,9 @@ import threading
 from threading import Thread
 from tkinter import filedialog
 import tqdm
-import copy
+import errno
+import re
+
 gui = None
 sock = None
 threads = []
@@ -18,11 +20,11 @@ BUFFER_SIZE = 4096
 SEPARATOR = "<SEPARATOR>"
 buffers = []
 
-# executed by other thread
-def make_download_button(text, filename):
-    gui.chat_log.window_create(tkinter.END, window=tkinter.Button(gui.chat_log, text=text, command=testfunc))
-    global makebutton
-    makebutton = False
+HOST = None
+PORT = None
+password = None
+username = None
+
 
 # executed by other thread
 def show_chat(msg):
@@ -33,9 +35,9 @@ def show_chat(msg):
     makebutton = True
     gui.chat_log.see("end")
 
-def show_file(filename):
+def show_file(filename, file_id, conv_filesize, filesize):
     global gui
-    gui.make_download_button(filename)
+    gui.make_download_button(filename, file_id, conv_filesize, filesize)
     gui.chat_log.insert(tkinter.END, '\n')
 
 def testfunc():
@@ -48,61 +50,140 @@ def send_file():
     filename = filepath.split('/')[-1]
     print('filename: ', filename)
     filesize = os.path.getsize(filepath)
-    sock.send(f'[FUP]{filename}{SEPARATOR}{filesize}[END]'.encode('ascii'))
-    progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    sock.sendall(f'[FUP]{filename}{SEPARATOR}{filesize}[END]'.encode('ascii'))
+    # progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
     send_times = int(filesize/BUFFER_SIZE) + 1
+    remaining = filesize
     with open(filepath, 'rb') as f:
         bytes_read = None
         for i in range(0, send_times):
             bytes_read = f.read(BUFFER_SIZE)
             sock.send(bytes_read)
-            progress.update(len(bytes_read))
+            # progress.update(len(bytes_read))
+        i = 0
+        # while True:
+        #     buff = None
+            # if remaining >= BUFFER_SIZE:
+            #     buff = BUFFER_SIZE
+            #     remaining = remaining - BUFFER_SIZE
+            # elif remaining == 0:
+            #     break
+            # elif remaining < BUFFER_SIZE:
+            #     buff = remaining
+            # else:
+            #     raise
+
+            # bytes_read = f.read(BUFFER_SIZE)
+            # # progress.update(len(bytes_read))
+            # if not bytes_read:
+            #     sock.sendall(b'$[%EOF%]$')
+            #     print()
+            #     print('done sending')
+            #     print(f'sent {i} times')
+            #     break
+            # sock.sendall(bytes_read)
+            # i = i + 1
         f.close()
 
-def receive_file(received):
-    global buffer
+def request_file(filename, file_id, file_size):
+    print(f'request {filename} id: {file_id} size: {file_size}')
+    request_filename = f'{file_id}-{filename}'
+    request = f'[FRQ]{request_filename}[END]'
+    filler_len = 4096 - len(request)
+    filler = ' ' * filler_len
+    full_request = request + filler
+    encoded_request = full_request.encode('ascii')
+    sock.send(encoded_request)
+
+def receive_file(filename, file_size):
+    global buffers
     global sock
-    filename, filesize = received.split(SEPARATOR)
-    filename = os.path.basename(filename)
-    filesize = int(filesize)
-    print(filesize)
-    recv_times = int(filesize / BUFFER_SIZE + 1)
-    if filesize == 0:
-        f = open(filename, "wb")
+
+    recv_times = int(int(file_size) / BUFFER_SIZE + 1)
+
+    remaining = file_size
+    print(f'must receive {recv_times} times!')
+    if not os.path.exists(os.path.dirname('downloads')):
+        try:
+            os.makedirs('downloads')
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+    if file_size == 0:
+        f = open('downloads/' +str(filename), "wb")
         f.close()
         return
     else:
-        with open(filename, "wb") as f:
-            for buffer in buffers:
-                encoded = buffer.encode('ascii')
-                f.write(encoded)
-            for i in range(0, recv_times):
-                bytes_read = sock.recv(BUFFER_SIZE)
-                f.write(bytes_read)
-            f.close()
+        with open('downloads/' + filename, "wb") as f:
+            eof = '$[%EOF%]$'
+            eof_fill_len = 4096 - len(eof)
+            eof_fill = ' ' * eof_fill_len
+            eof = eof + eof_fill
+            remaining_count = int(file_size)
+            bytes_read = None
+            while True:
+                if BUFFER_SIZE > remaining_count > 0:
 
+                    bytes_read = sock.recv(remaining_count)
+                    while len(bytes_read) != remaining_count:
+                        bytes_read = bytes_read + sock.recv(remaining_count - len(bytes_read))
+                    remaining_count = remaining_count - len(bytes_read)
+                else:
+                    print('check to receive last')
+                    bytes_read = sock.recv(BUFFER_SIZE)
+                    while len(bytes_read) != BUFFER_SIZE:
+                        bytes_read = bytes_read + sock.recv(BUFFER_SIZE - len(bytes_read))
+                    remaining_count = remaining_count - len(bytes_read)
+                # decoded_bytes_read = bytes_read.decode('ascii')
+                if bytes_read == eof.encode('utf-8'):
+                    print(f'breaking receivE!')
+                    break
+
+                f.write(bytes_read)
+
+            f.close()
 # to be executed by other thread
 def receive_chat():
     print("recv chat: " + str(threading.get_ident()))
+    recv_msg = None
     try:
         while True:
-            recv_msg = sock.recv(1024)
+            recv_msg = sock.recv(4096)
             decoded_recv_msg = recv_msg.decode('ascii')
-            header = decoded_recv_msg[0:5]
-            msg_body = decoded_recv_msg[5:]
+            decoded_recv_msg = decoded_recv_msg.rstrip()
+            usable_message = decoded_recv_msg.replace('[END]', '')
+            # split_messages = decoded_recv_msg.split('[END]')
+            # usable_message = split_messages[0]
+            # if len(split_messages) > 1:
+            #     print("more than one split message")
+            #     if split_messages[1] != '':
+            #         buffers.append(split_messages[1])
+            #         print(split_messages[1])
+            header = usable_message[0:5]
+            msg_body = usable_message[5:]
             if recv_msg == b'Server Closed':
                 show_chat("The server is closed")
                 break
             if header == '[MSG]':
                 show_chat(msg_body)
             elif header == '[FBC]':
-                broadcast_filename = msg_body
-                show_file(broadcast_filename)
+                split_body = msg_body.split(SEPARATOR)
+                broadcast_filename = split_body[0]
+                broadcast_fileid = split_body[1]
+                broadcast_conv_filesize = split_body[2]
+                broadcast_filesize = split_body[3]
+                show_file(broadcast_filename, broadcast_fileid, broadcast_conv_filesize, broadcast_filesize)
             elif header == '[FDN]':
-                None
+                print('header entered!')
+                split_body = msg_body.split(SEPARATOR)
+                download_filename = split_body[0]
+                download_size = split_body[1]
+                receive_file(download_filename, download_size)
 
     except Exception as e:
+        #print(f'receive message was: {recv_msg}')
         show_chat("An error occurred. Please restart the program.")
+
 
 # to be executed by main thread
 def send_chat(*args):
@@ -133,6 +214,7 @@ def connect_to_server():
     # PORT = gui.port_value.get()
     # password = gui.password_value.get()
     # username = gui.username_value.get()
+    global HOST, PORT, password, username
     HOST = '221.155.194.15'
     PORT = '50007'
     password = '1234'
@@ -193,6 +275,50 @@ def connect_to_server():
         # chat(sock)
 
         return 0
+    except socket.error as e:
+        print("outer error")
+        print(e)
+    except KeyboardInterrupt as e:
+        print("client closed by user")
+
+def new_sock():
+    newsock = None
+    # HOST = gui.server_ip_value.get()
+    # PORT = gui.port_value.get()
+    # password = gui.password_value.get()
+    # username = gui.username_value.get()
+    global HOST, PORT, password, username
+
+    try:
+        for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
+            af, socktype, proto, canonname, sa = res
+            # create socket
+            try:
+                newsock = socket.socket(af, socktype, proto)
+            except OSError as msg:
+                print("oserror1")
+                print(msg)
+                newsock = None
+                continue
+            # connect to server socket
+            try:
+                newsock.connect(sa)
+            except OSError as msg:
+                print("oserror2")
+                print(msg)
+                newsock.close()
+                newsock = None
+                continue
+            except socket.error as e:
+                newsock = None
+                print("socket")
+            # break for loop if connection was made for IPv4 or IPv6
+            break
+        # If sock is still None after for loop, connection to server failed
+        if newsock is None:
+            print('could not open socket')
+            return None
+        return newsock
     except socket.error as e:
         print("outer error")
         print(e)
@@ -355,9 +481,9 @@ class ChatGui:
         self.make_button.set(True)
 
 
-    def make_download_button(self, file_name):
+    def make_download_button(self, file_name, file_id, conv_filesize, file_size):
         print("make download: " + str(threading.get_ident()))
-        self.chat_log.window_create(tkinter.END, window=tkinter.Button(self.chat_log, text=f"Download {file_name}", command=lambda arg1=file_name: show_file(arg1)))
+        self.chat_log.window_create(tkinter.END, window=tkinter.Button(self.chat_log, text=f"Download {file_name} - {conv_filesize}", command=lambda arg1=file_name, arg2=file_id, arg3=file_size: request_file(arg1, arg2, arg3)))
         global makebutton
         makebutton = False
 
